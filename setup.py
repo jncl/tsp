1d29db6cb839babc3af3c93b5251f5cc55093404
Jon Hogg <hogg.jon+github@gmail.com>
2025-07-13 17:48:58 +0100

    Updated setup.py & pyproject.toml

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 """ setup.py """
 # vim: set fileencoding=utf-8:
 
-# from setuptools import setup
+from setuptools import setup
 
-# REQUIRES = [
-# ]
+REQUIRES = [
+]
 
-# setup(name="tsp",
-#       version="2.0",
-#       description="Task spooler",
-#       author="Jon Hogg",
-#       author_email="hogg.jon@gmail.com",
-#       packages=["tsp"],
-#       package_dir={"tsp": "tsp"},
-#       entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
-#       long_description="Inspired by ts",
-#       classifiers=["Intended Audience :: Developers"],
-#       keywords="tasks",
-#       license="Public Domain",
-#       install_requires=REQUIRES)
+setup(name="tsp",
+      version="2.0",
+      description="Task spooler",
+      author="Jon Hogg",
+      author_email="hogg.jon@gmail.com",
+      packages=["tsp"],
+      package_dir={"tsp": "tsp"},
+      entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
+      long_description="Inspired by ts",
+      classifiers=["Intended Audience :: Developers"],
+      keywords="tasks",
+      license="Public Domain",
+      install_requires=REQUIRES)
