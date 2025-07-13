394358bdf91f8ab28d8e7f09e85f0179c67cd3ed
Jon Hogg <hogg.jon+github@gmail.com>
2025-07-13 18:16:24 +0100

    Updated build config files

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@
 
 from setuptools import setup
 
-REQUIRES = [
-]
+setup()
 
-setup(name="tsp",
-      version="2.0",
-      description="Task spooler",
-      author="Jon Hogg",
-      author_email="hogg.jon@gmail.com",
-      packages=["tsp"],
-      package_dir={"tsp": "tsp"},
-      entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
-      long_description="Inspired by ts",
-      classifiers=["Intended Audience :: Developers"],
-      keywords="tasks",
-      license="CC-PDM-1.0",
-      install_requires=REQUIRES)
+# REQUIRES = [
+# ]
+
+# setup(name="tsp",
+#       version="2.0",
+#       description="Task spooler",
+#       author="Jon Hogg",
+#       author_email="hogg.jon@gmail.com",
+#       packages=["tsp"],
+#       package_dir={"tsp": "tsp"},
+#       entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
+#       long_description="Inspired by ts",
+#       classifiers=["Intended Audience :: Developers"],
+#       keywords="tasks",
+#       license="CC-PDM-1.0",
+#       install_requires=REQUIRES)
