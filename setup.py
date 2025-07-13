978614df8e05bccbff27d9e0a70ca70f830ea1b0
Jon Hogg <hogg.jon+github@gmail.com>
2025-07-13 17:44:19 +0100

    Updated Project structure for build system Added pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python
+""" setup.py """
 # vim: set fileencoding=utf-8:
 
-import datetime
-import os
-import sys
+# from setuptools import setup
 
-from setuptools import setup
+# REQUIRES = [
+# ]
 
-
-REQUIRES = [
-]
-
-
-setup(name="tsp",
-      version="1.0",
-      description="Task spooler",
-      author="Justin Forest",
-      author_email="hex@umonkey.net",
-      url="https://umonkey.net/",
-      packages=["tsp"],
-      package_dir={"tsp": "tsp"},
-      #scripts=["extras/task-pull", "extras/task-push"],
-      entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
-      long_description="Inspired by ts",
-      classifiers=["Intended Audience :: Developers"],
-      keywords="tasks",
-      license="Public Domain",
-      install_requires=REQUIRES)
+# setup(name="tsp",
+#       version="2.0",
+#       description="Task spooler",
+#       author="Jon Hogg",
+#       author_email="hogg.jon@gmail.com",
+#       packages=["tsp"],
+#       package_dir={"tsp": "tsp"},
+#       entry_points={"console_scripts": ["tsp=tsp.cli:main"]},
+#       long_description="Inspired by ts",
+#       classifiers=["Intended Audience :: Developers"],
+#       keywords="tasks",
+#       license="Public Domain",
+#       install_requires=REQUIRES)
